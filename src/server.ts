4a497f2291bdfccb40cdc49a4d5795c63e18bf34
Linus Gellhorn <linus.g@hotmail.com>
2021-12-01 16:26:59 +0000

    Added basic todo CRUD endpoints, without failing responses

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import { Client } from "pg";
 import dotenv from "dotenv";
 import {
   addDummyDbItems,
@@ -11,6 +12,10 @@ import {
   deleteDbItemById,
 } from "./db";
 import filePath from "./filePath";
+
+const client = new Client({ database: "todoApp" });
+
+client.connect();
 
 // loading in some dummy items into the database
 // (comment out if desired, or change the number)
@@ -35,50 +40,106 @@ app.get("/", (req, res) => {
   res.sendFile(pathToFile);
 });
 
-// GET /todos
-app.get("/todos", (req, res) => {
-  const allTodos = getAllDbItems();
-  res.status(200).json(allTodos);
-});
-
-// POST /todos
-app.post<{}, {}, DbItem>("/todos", (req, res) => {
-  // to be rigorous, ought to handle non-conforming request bodies
-  // ... but omitting this as a simplification
-  const postData = req.body;
-  const createdTodo = addDbItem(postData);
-  res.status(201).json(createdTodo);
-});
-
-// GET /todos/:id
-app.get<{ id: string }>("/todos/:id", (req, res) => {
-  const matchingTodo = getDbItemById(parseInt(req.params.id));
-  if (matchingTodo === "not found") {
-    res.status(404).json(matchingTodo);
-  } else {
-    res.status(200).json(matchingTodo);
+// Get all todos
+app.get("/todos", async (req, res) => {
+  try {
+    const allTodos = await client.query("SELECT * FROM todo");
+    res.status(200).json(allTodos.rows);
+  } catch (err) {
+    console.error(err);
   }
 });
+// app.get("/todos", (req, res) => {
+//   const allTodos = getAllDbItems();
+//   res.status(200).json(allTodos);
+// });
 
-// DELETE /todos/:id
-app.delete<{ id: string }>("/todos/:id", (req, res) => {
-  const matchingTodo = deleteDbItemById(parseInt(req.params.id));
-  if (matchingTodo === "not found") {
-    res.status(404).json(matchingTodo);
-  } else {
-    res.status(200).json(matchingTodo);
+// Post a todo
+app.post("/todos", async (req, res) => {
+  try {
+    const { description } = req.body;
+    const newTodo = await client.query(
+      "INSERT INTO todo (description) VALUES ($1) RETURNING *",
+      [description]
+    );
+    res.status(201).json({
+      status: "success",
+      newTodo: newTodo.rows[0],
+    });
+  } catch (err) {
+    console.error(err);
   }
 });
+// app.post<{}, {}, DbItem>("/todos", (req, res) => {
+//   // to be rigorous, ought to handle non-conforming request bodies
+//   // ... but omitting this as a simplification
+//   const postData = req.body;
+//   const createdTodo = addDbItem(postData);
+//   res.status(201).json(createdTodo);
+// });
 
-// PATCH /todos/:id
-app.patch<{ id: string }, {}, Partial<DbItem>>("/todos/:id", (req, res) => {
-  const matchingTodo = updateDbItemById(parseInt(req.params.id), req.body);
-  if (matchingTodo === "not found") {
-    res.status(404).json(matchingTodo);
-  } else {
-    res.status(200).json(matchingTodo);
+// Get a specific todo
+app.get("/todos/:id", async (req, res) => {
+  try {
+    const id = parseInt(req.params.id);
+    const matchingTodo = await client.query(
+      "SELECT * FROM todo WHERE id = $1",
+      [id]
+    );
+    res.status(200).json(matchingTodo.rows[0]);
+  } catch (err) {
+    console.error(err);
   }
 });
+// app.get<{ id: string }>("/todos/:id", (req, res) => {
+//   const matchingTodo = getDbItemById(parseInt(req.params.id));
+//   if (matchingTodo === "not found") {
+//     res.status(404).json(matchingTodo);
+//   } else {
+//     res.status(200).json(matchingTodo);
+//   }
+// });
+
+// Delete a todo
+app.delete("/todos/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const deleteTodo = await client.query(
+      "DELETE FROM todo WHERE id = $1 RETURNING *",
+      [id]
+    );
+    res.status(200).json(deleteTodo.rows[0]);
+  } catch (err) {
+    console.error(err);
+  }
+});
+// app.delete<{ id: string }>("/todos/:id", (req, res) => {
+//   const matchingTodo = deleteDbItemById(parseInt(req.params.id));
+//   if (matchingTodo === "not found") {
+//     res.status(404).json(matchingTodo);
+//   } else {
+//     res.status(200).json(matchingTodo);
+//   }
+// });
+
+// Update a todo
+app.patch("/todos/:id", async (req, res) => {
+  const { description } = req.body;
+  const id = parseInt(req.params.id);
+  const updateTodo = await client.query(
+    "UPDATE todo SET description = $1 WHERE id = $2 RETURNING *",
+    [description, id]
+  );
+  res.status(200).json(updateTodo.rows[0]);
+});
+// app.patch<{ id: string }, {}, Partial<DbItem>>("/todos/:id", (req, res) => {
+//   const matchingTodo = updateDbItemById(parseInt(req.params.id), req.body);
+//   if (matchingTodo === "not found") {
+//     res.status(404).json(matchingTodo);
+//   } else {
+//     res.status(200).json(matchingTodo);
+//   }
+// });
 
 app.listen(PORT_NUMBER, () => {
   console.log(`Server is listening on port ${PORT_NUMBER}!`);
