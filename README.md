f52b2bfd0ed9ae5ebe4cae34f48e94466628e05f
Linus Gellhorn <linus.g@hotmail.com>
2021-12-28 17:15:23 +0000

    Final code cleanup, updated readme

--- README.md
+++ README.md
@@ -4,21 +4,24 @@ This is set up as a quick starter for a no-persistence RESTful Express API (usin
 
 ## Getting started
 
-### Shaping the 'database'
-
-`db.ts` is set up for a generic `DbItem` - you may want to reshape and rename that interface.
-
 ### Install dependencies
+
 ```
 yarn
 ```
 
 ### Run server
+
 ```
 yarn start
 ```
 
 Or, for dev mode (restarts server automatically when relevant files change):
+
 ```
 yarn start:dev
 ```
+
+### Heroku app
+
+Access the backend API hosted on Heroku [here](https://todo-app-academy.herokuapp.com/)
--- src/server.ts
+++ src/server.ts
@@ -9,9 +9,6 @@ config(); //Read .env file lines as though they were env vars.
 //Call this script with the environment variable LOCAL set if you want to connect to a local db (i.e. without SSL)
 //Do not set the environment variable LOCAL if you want to connect to a heroku DB.
 
-//For the ssl property of the DB connection config, use a value of...
-// false - when connecting to a local DB
-// { rejectUnauthorized: false } - when connecting to a heroku DB
 const herokuSSLSetting = { rejectUnauthorized: false };
 const sslSetting = process.env.LOCAL ? false : herokuSSLSetting;
 const dbConfig = {
